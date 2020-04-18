3e0a7fe0f93035c558ce7fa8b3dce1dbbd92f4ab
Topher Brennan <topher@tophers-air.attlocal.net>
2020-04-17 20:10:29 -0700

    Added another analytic

--- covid_analysis.rb
+++ covid_analysis.rb
@@ -1,32 +1,27 @@
 require "open-uri"
 
 GLOBAL_DEATHS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+GLOBAL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 UNITED_STATES = "US"
 ITALY = "Italy"
 DATE_REGEX = /\d\d?\/\d\d?\/\d\d/
 
 # Stolen from https://stackoverflow.com/a/57397583
 # Use over other solutions because I don't want to put every array element on a separate line
-module PrettyHash
-  # Usage: PrettyHash.call(nested_hash)
-  # Prints the nested hash in the easy to look on format
-  # Returns the amount of all values in the nested hash
-
-  def self.call(hash, level: 0, indent: 2)
-    unique_values_count = 0
-    hash.each do |k, v|
-      (level * indent).times { print ' ' }
-      print "#{k}:"
-      if v.is_a?(Hash)
-        puts
-        unique_values_count += call(v, level: level + 1, indent: indent)
-      else
-        puts " #{v}"
-        unique_values_count += 1
-      end
+def pretty_hash(hash, level: 0, indent: 2)
+  unique_values_count = 0
+  hash.each do |k, v|
+    (level * indent).times { print ' ' }
+    print "#{k}:"
+    if v.is_a?(Hash)
+      puts
+      unique_values_count += pretty_hash(v, level: level + 1, indent: indent)
+    else
+      puts " #{v}"
+      unique_values_count += 1
     end
-    unique_values_count
   end
+  unique_values_count
 end
 
 def parse_csv_from_uri(uri)
@@ -55,7 +50,21 @@ def weekly_factors(rows, country)
   results = []
   while i < deaths.size
     result = 0
-    result = (deaths[i].to_f / deaths[i-7].to_f).round(1) if deaths[i-7].to_f > 0
+    result = (deaths[i].to_f / deaths[i-7].to_f).round(2) if deaths[i-7].to_f > 0
+    results << result
+    i += 1
+  end
+  results
+end
+
+def weekly_change_in_growth(rows, country)
+  growth = weekly_growth(rows, country)
+  
+  i = 7
+  results = []
+  while i < growth.size
+    result = 0
+    result = (growth[i].to_f / growth[i-7].to_f).round(2) if growth[i-7].to_f > 0
     results << result
     i += 1
   end
@@ -66,8 +75,15 @@ if __FILE__ == $PROGRAM_NAME
   rows = parse_csv_from_uri(GLOBAL_DEATHS)
 
   results = {
-    italy: weekly_growth(rows, ITALY),
-    united_states: weekly_growth(rows, UNITED_STATES)
+    growth: {
+      italy: weekly_growth(rows, ITALY),
+      united_states: weekly_growth(rows, UNITED_STATES)
+    },
+    change_in_growth: {
+      italy: weekly_change_in_growth(rows, ITALY),
+      united_states: weekly_change_in_growth(rows, UNITED_STATES)
+    }
   }
-  print PrettyHash.call(results)
+  print pretty_hash(results)
+  print "\n"
 end
