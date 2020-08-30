a503a16427ba1298d7257aeefa2f0531f7534f87
Topher Brennan <topherbrennan@tophers-mini.attlocal.net>
2020-08-30 13:14:33 -0700

    Added scripts to visualize Alameda County data.

---
+++ county_plot.py
@@ -0,0 +1,24 @@
+import csv
+import urllib.request
+import matplotlib.pyplot as plt
+
+plt.ion()
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+response = urllib.request.urlopen(url)
+cr = csv.reader(response.read().decode('utf-8').splitlines())
+
+def plot(data):
+    x_axis = list(range(len(data)))
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, data)
+
+for row in cr:
+    if 'Alameda' in row and 'California' in row:
+        first_case_index = row.index('1')
+        case_data = [int(el) for el in row[first_case_index:]]
+        l = len(case_data)
+        new_cases = [case_data[i+7] - case_data[i] for i in range(l - 7)]
+        plot(case_data)
+        plot(new_cases)
+        k=input("press any key to exit")
+
